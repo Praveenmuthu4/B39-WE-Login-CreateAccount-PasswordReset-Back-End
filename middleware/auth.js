fdc694cbd547aca6810dae1189eb701eb31e673b
Praveenmuthu4 <praveenmuthu4@gmail.com>
2023-04-01 19:29:26 +0530

    reconfigured back all data

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,29 +1,11 @@
-const jwt = require("jsonwebtoken");
-const userdb = require("../models/userSchema");
-const keysecret = process.env.SECRET_KEY
+import jwt from "jsonwebtoken";
 
-
-const authenticate = async(req,res,next)=>{
-
-    try {
-        const token = req.headers.authorization;
-        
-        const verifytoken = jwt.verify(token,keysecret);
-        
-        const rootUser = await userdb.findOne({_id:verifytoken._id});
-        
-        if(!rootUser) {throw new Error("user not found")}
-
-        req.token = token
-        req.rootUser = rootUser
-        req.userId = rootUser._id
-
-        next();
-
-    } catch (error) {
-        res.status(401).json({status:401,message:"Unauthorized no token provide"})
-    }
-}
-
-
-module.exports = authenticate
+export const auth = (req, res, next) => {
+  try {
+    const token = req.header("x-auth-token");
+    jwt.verify(token, process.env.SECRET_KEY);
+    next();
+  } catch (err) {
+    res.send({ error: err.message });
+  }
+};
