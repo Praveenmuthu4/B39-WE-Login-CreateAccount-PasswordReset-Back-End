ba49287236e65778608dc4062b5b28927c2b14cd
Praveenmuthu4 <praveenmuthu4@gmail.com>
2023-04-01 18:33:25 +0530

    some changes done in the backend

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,29 @@
-import jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
+const userdb = require("../models/userSchema");
+const keysecret = process.env.SECRET_KEY
 
-export const auth = (req, res, next) => {
-  try {
-    const token = req.header("x-auth-token");
-    jwt.verify(token, process.env.SECRET_KEY);
-    next();
-  } catch (err) {
-    res.send({ error: err.message });
-  }
-};
+
+const authenticate = async(req,res,next)=>{
+
+    try {
+        const token = req.headers.authorization;
+        
+        const verifytoken = jwt.verify(token,keysecret);
+        
+        const rootUser = await userdb.findOne({_id:verifytoken._id});
+        
+        if(!rootUser) {throw new Error("user not found")}
+
+        req.token = token
+        req.rootUser = rootUser
+        req.userId = rootUser._id
+
+        next();
+
+    } catch (error) {
+        res.status(401).json({status:401,message:"Unauthorized no token provide"})
+    }
+}
+
+
+module.exports = authenticate
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,18 @@ import {
   getUserByName,
   getUserList,
 } from "../helper.js";
+const userFromDb = require("../models/userSchema.js")
+const authenticate = require("../middleware/auth.js");
+const nodemailer = require("nodemailer");
 const router = express.Router();
+
+const transporter = nodemailer.createTransport({
+  service:"gmail",
+  auth:{
+      user:process.env.EMAIL,
+      pass:process.env.PASSWORD
+  }
+}) 
 
 
 router.post("/signup", async (req, res) => {
@@ -59,72 +70,164 @@ router.get("/list", async (req, res) => {
   res.send(result);
 });
 
-router.get("/forget-password", (req, res, next) => {
-  res.render("forget-password");
-});
+// //forget-password
 
-//forget-password
+// router.post("/forget-password",async (req, res, next) => {
+//   const { Email } = req.body;
+//   const userFromDb = await getUserByName(Email);
+//   console.log(userFromDb)
+//   if (!userFromDb) {
+//     res.status(400).send({message:"User details not registered"});
+//     return;
+//   }
+//   const secret = process.env.SECRET_KEY + userFromDb.password;
+//   const payload = {
+//     Email: userFromDb.Email,
+//   };
+//   const token = jwt.sign(payload, secret, { expiresIn: "15m" });
+//   const localhost = `http://localhost:4000/reset-password/${userFromDb.Email}/${token}`;
+//   const link =  localhost
+//   console.log(link);
+//   res.send(
+//     "Password reset link has been sent to your email ID and Its valid for 15 minutes only"
+//   );
+// });
 
-router.post("/forget-password",async (req, res, next) => {
-  const { Email } = req.body;
-  const userFromDb = await getUserByName(Email);
-  console.log(userFromDb)
-  if (!userFromDb) {
-    res.status(400).send({message:"User details not registered"});
-    return;
+// //reset-password
+// router.get("/reset-password/:Email/:token",async (req, res, next) => {
+//   const { Email, token } = req.params;
+//   const userFromDb = await getUserByName(Email);
+//   if (!userFromDb) {
+//     res.send("Invalid ID");
+//     return;
+//   }
+//   const secret = process.env.SECRET_KEY + userFromDb.password;
+//   try {
+//     const payload = jwt.verify(token, secret);
+//     res.render( { Email: userFromDb.Email });
+//   } catch (error) {
+//     console.log(error.message);
+//     res.send(error.message);
+//   }
+// });
+
+// router.post("/reset-password/:Email/:token",async (req, res, next) => {
+//   const { Email, token } = req.params;
+//   const storedDbPassword = userFromDb.password;
+//   const isPasswordMatch = await bcrypt.compare(password, storedDbPassword);
+
+//   const { password} = req.body;
+
+//   if (Email !== userFromDb.Email) {
+//     res.send("Invalid ID");
+//     return;
+//   }
+//   const secret = process.env.SECRET_KEY + user.password;
+//   try {
+//     const payload = jwt.verify(token, secret);
+//     userFromDb.password = password;
+//     res.send(user);
+//   } catch (error) {
+//     console.log(error.message);
+//     res.send(error.message);
+//   }
+// });
+
+
+router.post("/reset-password",async(req,res)=>{
+  console.log(req.body)
+
+  const {Email} = req.body;
+
+  if(!Email){
+      res.status(401).json({status:401,message:"Enter Your Email"})
   }
-  const secret = process.env.SECRET_KEY + userFromDb.password;
-  const payload = {
-    Email: userFromDb.Email,
-  };
-  const token = jwt.sign(payload, secret, { expiresIn: "15m" });
-  const localhost = `http://localhost:4000/reset-password/${userFromDb.Email}/${token}`;
-  const link =  localhost
-  console.log(link);
-  res.send(
-    "Password reset link has been sent to your email ID and Its valid for 15 minutes only"
-  );
-});
 
-//reset-password
-router.get("/reset-password/:Email/:token",async (req, res, next) => {
-  const { Email, token } = req.params;
-  const userFromDb = await getUserByName(Email);
-  if (!userFromDb) {
-    res.send("Invalid ID");
-    return;
-  }
-  const secret = process.env.SECRET_KEY + userFromDb.password;
   try {
-    const payload = jwt.verify(token, secret);
-    res.render( { Email: userFromDb.Email });
+      const userFind = await users.findOne({email:email});
+
+      // token generate for reset password
+      const token = jwt.sign({_id:userFromDb._id},keysecret,{
+          expiresIn:"120s"
+      });
+      
+      const setusertoken = await users.findByIdAndUpdate({Email:userFind.Email},{verifytoken:token},{new:true});
+
+
+      if(setusertoken){
+          const mailOptions = {
+              from:process.env.EMAIL,
+              to:Email,
+              subject:"Sending Email For password Reset",
+              text:`This Link Valid For 2 MINUTES http://localhost:3001/forgotpassword/${userFromDb.Email}/${setusertoken.verifytoken}`
+          }
+
+          transporter.sendMail(mailOptions,(error,info)=>{
+              if(error){
+                  console.log("error",error);
+                  res.status(401).json({status:401,message:"email not send"})
+              }else{
+                  console.log("Email sent",info.response);
+                  res.status(201).json({status:201,message:"Email sent Succsfully"})
+              }
+          })
+
+      }
+
   } catch (error) {
-    console.log(error.message);
-    res.send(error.message);
+      res.status(401).json({status:401,message:"invalid user"})
   }
+
 });
 
-router.post("/reset-password/:Email/:token",async (req, res, next) => {
-  const { Email, token } = req.params;
-  const storedDbPassword = userFromDb.password;
-  const isPasswordMatch = await bcrypt.compare(password, storedDbPassword);
+router.get("/forgot-password/:Email/:token",async(req,res)=>{
+  const {Email,token} = req.params;
 
-  const { password} = req.body;
-
-  if (Email !== userFromDb.Email) {
-    res.send("Invalid ID");
-    return;
-  }
-  const secret = process.env.SECRET_KEY + user.password;
   try {
-    const payload = jwt.verify(token, secret);
-    userFromDb.password = password;
-    res.send(user);
+      const validuser = await users.findOne({Email:Email,verifytoken:token});
+      
+      const verifyToken = jwt.verify(token,keysecret);
+
+      console.log(verifyToken)
+
+      if(validuser && verifyToken.Email){
+          res.status(201).json({status:201,validuser})
+      }else{
+          res.status(401).json({status:401,message:"user not exist"})
+      }
+
   } catch (error) {
-    console.log(error.message);
-    res.send(error.message);
+      res.status(401).json({status:401,error})
   }
 });
+
+router.post("/:Email/:token",async(req,res)=>{
+  const {Email,token} = req.params;
+
+  const {password} = req.body;
+
+  try {
+      const validuser = await userdb.findOne({Email:Email,verifytoken:token});
+      
+      const verifyToken = jwt.verify(token,keysecret);
+
+      if(validuser && verifyToken.Email){
+          const newpassword = await bcrypt.hash(password,12);
+
+          const setnewuserpass = await userdb.findByIdAndUpdate({Email:Email},{password:newpassword});
+
+          setnewuserpass.save();
+          res.status(201).json({status:201,setnewuserpass})
+
+      }else{
+          res.status(401).json({status:401,message:"user not exist"})
+      }
+  } catch (error) {
+      res.status(401).json({status:401,error})
+  }
+})
+
+
 
 export const userRouter = router;
 
